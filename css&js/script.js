d2dcdeffab9d0eac6fa6981ef142e9e07eb29495
Ismail Dedec <ismaildedec@gmail.com>
2020-12-08 19:30:40 +0300

    htmlcalismalari02 eklendi

---
+++ css&js/script.js
@@ -0,0 +1,15 @@
+ 
+ function mesajGonder() {
+ alert ("HOŞ GELDİNİZ");
+ };
+
+ function mesajGonder(deger) {
+    var p=document.getElementById('link1');
+     p.textContent=deger;
+    };
+
+ function degistirme(){
+     var p=document.getElementById('link1');
+     p.textContent='Buton Ile Degisti';
+ 
+ };
